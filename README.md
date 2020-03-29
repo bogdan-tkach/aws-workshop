f46a769fdc847e6a787013037b307d930880d73d
Bogdan Tkach <bagedan@gmail.com>
2020-03-29 23:03:13 +0800

    Ad cloud formation scripts to create workshop scenario. Current state: api gateway is integrated with lambda nad allowed to call it. Latest lambda code is uploaded as part of packaging TODO: test lambda to DB set up

--- README.md
+++ README.md
@@ -14,3 +14,8 @@ AWS API Gateway allows only one removal per minute, which makes cleaning up afte
 
 
 Use aws-clean-up/index.js to remove all apis and dynamodb tables created in all regions. 
+
+## Cloudformation 
+./run-cf.sh package-lambda - package lambda in zip and push to S3. result is generated CF template in cloudformation/build folder with link to S3 
+./run-cf.sh create-stack - create stack with CF template generated in prev step 
+./run-cf.sh delete-stack - delete stack 
---
+++ cloudformation/run-cf.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ $# -lt 1 ]
+then
+    echo "Usage : $0 package-lambda | create-stack | delete-stack"
+    exit
+fi
+
+case "$1" in
+'package-lambda') aws2 cloudformation package --template workshop-template-original.yaml \
+--s3-bucket btkach-cloud-formation-playground > build/workshop-template-packaged.yaml
+;;
+'create-stack') aws2 cloudformation create-stack --stack-name dynamo-test --template-body file://build/workshop-template-packaged.yaml \
+--role-arn arn:aws:iam::903167650995:role/WorkshopWithCloudFoundry --capabilities CAPABILITY_IAM CAPABILITY_AUTO_EXPAND
+;;
+'delete-stack') aws2 cloudformation delete-stack --stack-name dynamo-test
+;;
+'list-stacks') aws2 cloudformation list-stacks
+;;
+*) echo "Unsupported parameter $1"
+;;
+esac
